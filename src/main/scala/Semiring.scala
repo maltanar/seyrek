e3ae0ea56a81298489f0ded170ccebaf05e33a95
Yaman Umuroglu <maltanar@gmail.com>
2015-11-27 01:52:35 +0100

    bump tidbits + start working on simple pipelined 64-bit multiplier

--- src/main/scala/Semiring.scala
+++ src/main/scala/Semiring.scala
@@ -76,6 +76,46 @@ class SystolicReg(w: Int) extends Module {
   }
 }
 
+class PipelinedInt64Mul extends SemiringOp(64) {
+  val latency = 3
+  // TODO add support for flow control -- systolic regs or handshaking
+  // across latency
+  // TODO better/more parametrized structure
+
+  // convenience mux generator for turning two's complement numbers into
+  // magnitude (return value) + sign (highest bit of input)
+  def twosComplToMagnitude(in: UInt, w: Int): UInt = {
+    Mux(in(w-1), UInt(~in + UInt(1), width = w), in)
+  }
+  // turn into sign+magnitude form and register inputs
+  val regSignA = ShiftRegister(in = io.in.bits.first(63), n = 3)
+  val regSignB = ShiftRegister(in = io.in.bits.second(63), n = 3)
+  val regValA = Reg(next = twosComplToMagnitude(io.in.bits.first, 64))
+  val regValB = Reg(next = twosComplToMagnitude(io.in.bits.second, 64))
+  // useful to have the upper-lower parts of words
+  val a0 = regValA(31, 0)
+  val a1 = regValA(63, 32)
+  val b0 = regValB(31, 0)
+  val b1 = regValB(63, 32)
+  // compute the partial products
+  val regValA0B0 = Reg(init = UInt(0, width = 64), next = a0 * b0)
+  // a0b1 and a1b0 are truncated to 32 bits, since these will be left-shifted
+  // 32 bits before the reduction
+  val regValA0B1 = Reg(init = UInt(0, width = 32), next = a0 * b1)
+  val regValA1B0 = Reg(init = UInt(0, width = 32), next = a1 * b0)
+  // don't calculate a1b1 since we don't keep track of overflow bits
+
+  val resLower = regValA0B0(31, 0)
+  val resUpper = regValA0B0(63, 32) + regValA0B1 + regValA1B0
+  val magnRes = Cat(UInt(0, width=1), resUpper(30, 0), resLower)
+  val regMagnRes = Reg(init = UInt(0, width = 64), next = magnRes)
+
+  val isResultNegative = regSignA ^ regSignB
+  val res = Mux(isResultNegative, ~regMagnRes + UInt(1), regMagnRes)
+
+  io.out.bits := res
+}
+
 // generate operator (defined by fxn) with n-cycle latency
 // mostly intended to simulate high-latency ops, won't make much sense
 // in synthesis since all "useful work" is carried out before entering
